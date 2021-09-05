082c9c123a6ffd951e9677c3f006930fb891dea7
Ryan McCarthy <rmmcc92@aol.com>
2021-09-05 17:49:42 -0400

    fixed a few typos causing errors during testing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'No category found'});
       return;
     }
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -46,13 +46,16 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create(req.body, {
+   
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
+            category_id: req.body.category_id,
             tag_id,
           };
         });
